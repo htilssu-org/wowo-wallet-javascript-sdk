00deacd2e8af733cf93de03c39a8b65ff2c9b8a9
htilssu <tolashuu@gmail.com>
2024-10-09 19:21:23 +0700

    update sso redirect

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * from "./libs/WowoWallet";
 export * from "./libs/express/express_router";
-export * from "././libs/express/express_router"
+export * from "./libs/SSO";
+export * from './libs/utils/index';
--- libs/SSO.ts
+++ libs/SSO.ts
@@ -5,11 +5,19 @@ export const baseUrl = "https://sso.htilssu.id.vn";
 export class SSO {
     private baseUrl: string = baseUrl;
     private readonly jwk
+    private readonly serviceId: string;
 
-    constructor() {
+    constructor(serviceId: string) {
         this.jwk = jose.createRemoteJWKSet(new URL(`${this.baseUrl}/v1/sso/certs`))
+        this.serviceId = serviceId;
     }
 
+    /**
+     * Xác thực token trả về từ SSO server, và trả về payload chứa thông tin của người dùng nếu token hợp lệ,
+     * nếu token không hợp lệ giá trị trả về sẽ là `null`
+     * @param token token cần xác thực
+     * @returns null nếu token không hợp lệ hoặc payload của token
+     */
     public async verify(token: string) {
         try {
             return await jose.jwtVerify(token, this.jwk);
@@ -18,8 +26,19 @@ export class SSO {
         }
     }
 
-    public redirectToLogin(){
-
+    /**
+     * Chuyển hướng tới trang đăng nhập của server SSO với url trả về sau khi đăng nhập
+     * khi server đăng nhập xong sẽ chuyển hướng tới url trả về và kèm theo Token của người dùng từ searchParams
+     * @example
+     * ```js
+     * redirectToLoginPage("http://localhost:3000")
+     * ```
+     * Sau khi xác thực xong người dùng sẽ được chuyển hướng tới `returnUrl`
+     * `http://localhost:3000?Token=... các dịch vụ liên kết sẽ lấy token từ query params và lưu vào cookie của app`
+     * @param returnUrl sau khi đăng nhập xong server sso sẽ chuyển hướng tới
+     */
+    public redirectToLogin(returnUrl?: string) {
+        location.href = `${this.baseUrl}/sign-in?returnUrl=${returnUrl ? returnUrl : location}&serviceId=${this.serviceId}`
     }
 }
 
